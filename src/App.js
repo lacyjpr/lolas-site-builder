dcc3bd06fcfe8a58aca939ad26fdf3c6bbce3b15
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-09-13 05:10:18 -0700

    all the things reararraged

--- src/App.js
+++ src/App.js
@@ -11,6 +11,25 @@ class App extends Component {
           <h1 className="App-title">LOLa's Sitebulder</h1>
         </header>
         <p className="App-intro">Grandma got run over by a reindeer...</p>
+        <p className="App-intro">Speaking patois they call 'Tagalog'...</p>
+        <p className="App-intro">They pronounce it "Tag - Uh - Lawg"</p>
+        <p className="App-intro">Cuz they are haoles they don't know</p>
+        <p className="App-intro">We pronounce it 'Tagalog'</p>
+        <p className="App-intro">Which don't work but 'I know this'</p>
+        <p className="App-intro">Spitting this stuff in my room</p>
+        <p className="App-intro">Cause it's fun fun Mom!</p>
+        <p className="App-intro">Need a site, make a site...</p>
+        <p className="App-intro">Build it. Build it. Buildit.</p>
+        <p className="App-intro">"They call her Lola!!!"</p>
+        <p className="App-intro">I could go on but no screen</p>
+        <p className="App-intro">left</p>
+        <p className="App-intro">
+          [whisper]fits perfect on myscreen![/whisper]
+        </p>
+        <p className="App-intro">
+          [whisper] I love reading reflexive text jokes. Genious. Like the
+          "Uncommitted changes" joke ... [/whisper]
+        </p>
       </div>
     );
   }
